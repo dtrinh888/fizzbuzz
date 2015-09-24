3afe0a363a3ffc86d67b76c5078111b6bde46a7a
dtrinh888 <dtrinh888@gmail.com>
2015-09-23 22:13:24 -0700

    fizzbuzz prompt

---
+++ js/app.js
@@ -0,0 +1,22 @@
+function enterNumber(n) {
+
+	while (isNaN(parseInt(n))){
+		n = parseInt(prompt("Please enter a number: "));
+	}
+	
+	for(var i = 1; i <= n; i++) {
+		if (i % 15 === 0) {
+			document.write("Fizz Buzz" + "<br>");
+			continue;
+		} else if (i % 3 === 0) {
+			document.write("Fizz" + "<br>");
+			continue;
+		} else if (i % 5 === 0) {
+			document.write("Buzz" + "<br>");
+			continue;
+		}
+			document.write(i + "<br>");
+		}
+
+};
+enterNumber();
